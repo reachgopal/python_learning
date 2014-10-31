5ed9a9500a094e2a1d9c227e9a5197825246faab
Gopal <rgopal@fifthgentech.com>
2014-10-31 22:55:18 +0530

    test file

---
+++ p9.py
@@ -0,0 +1,23 @@
+import os
+os.system ("clear")
+
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJuly"
+
+print "Here is the days : ", days
+print "Here is the months : ", months
+
+print "." * 100
+
+print """
+
+there is something wrong here?
+
+"""
+
+print "." * 100
+
+print ""
+print ""
+
